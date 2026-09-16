84b4fc83556fd5652e9f5d6e352094a5a658bfb2
codergangganesh <mannamganeshbabu8@gmail.com>
2026-09-16 16:19:46 +0530

    Updated Features and Oauth for Github Impplemented.

--- src/components/ideas/IdeaEditorModal.jsx
+++ src/components/ideas/IdeaEditorModal.jsx
@@ -78,6 +78,7 @@ export function IdeaEditorModal({
       onClose={onClose}
       title={idea ? 'Edit Idea' : 'New Idea'}
       maxWidth="680px"
+      drawerMobile={true}
     >
       <form onSubmit={handleSubmit}>
         {/* Title & Favorite Toggle */}
